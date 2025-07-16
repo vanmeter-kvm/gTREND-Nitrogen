5a1af4d3c7445fcfa8b17efebab687353e24406c
Shuyu Chang <sxc6234@psu.edu>
2025-07-16 14:32:19 -0400

    verify extraction func works

--- Scripts/3Application/EXTRACT_gTREND.py
+++ Scripts/3Application/EXTRACT_gTREND.py
@@ -1,22 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 18 20:56:51 2024
 
-@author: sxc6234
-"""
+#gTREND-Nitrogen - Long-term nitrogen mass balance data for the contiguous United States (1930-2017)
+##As part of the gTREND-Nitrogen data package, we also provide a script on the project homepage that can be used to aggregate the gridded data to watershed or other administrative boundaries.
 
 import pandas as pd
-import geopands as gpd
+import geopandas as gpd
+from rasterstats import zonal_stats
 
-def extract_wshd_stats(wshd_path, gtrend_path,):
+
+def extract_wshd_stats(wshd_path, gtrend_path,var):
     wshd=gpd.read_file(wshd_path)
-    df_wshd=wshd.drop(subset="Geometry")
+    if wshd.crs.to_epsg() != 5070:
+        wshd = wshd.to_crs(epsg=5070)
+        
+    df_wshd=wshd.drop(columns=["geometry"])
     n=zonal_stats(wshd, gtrend_path, stats="mean")
     n = pd.DataFrame(n)
     df_wshd[var]=n["mean"].values
     return(df_wshd)
 
-
+#boundary path
 wshd_path=
+
+#gTREND-N raster path
 gtrend_path=
-extract_wshd_stats(wshd_path, gtrend_path,)
+var=
+
+#A dataframe stores the aggregated N values
+df_wshd=extract_wshd_stats(wshd_path, gtrend_path,var)
